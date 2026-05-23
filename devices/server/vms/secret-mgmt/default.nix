b8eed353ccb3bca9efe092df269310be6712e642
zeus <sensei@gumbolabs.xyz>
2026-05-22 21:53:30 -0500

    new VM to use for secret management

---
+++ devices/server/vms/secret-mgmt/default.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../../modules/nixvim.nix
+  ];
+
+  age.identityPaths = [ "/home/gumbo/.ssh/agenix-master" ];
+
+  users.users.gumbo = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    initialPassword = "supersecretpassword";
+    extraGroups = [
+      "wheel"
+      "docker"
+      "networkmanager"
+      "sound"
+      "video"
+      "audio"
+    ];
+  };
+
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    device = "nodev";
+  };
+
+  networking.hostName = "secret-mgmt";
+
+  server.baseline.enable = true;
+
+  workstation = {
+    ssh.enable = true;
+    nixvim.enable = true;
+  };
+}
+
