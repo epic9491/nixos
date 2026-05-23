43d04607deca0f0a7225229de839291f0b99abdc
zeus <sensei@gumbolabs.xyz>
2026-05-22 23:33:30 -0500

    add terraform builds for k3-1 and k3-2

---
+++ terra/k3-1.tf
@@ -0,0 +1,15 @@
+locals {
+  k3-1_ipv4 = "0.0.0.0"
+}
+
+module "deploy_k3-1" {
+  source                 = "github.com/nix-community/nixos-anywhere//terraform/all-in-one"
+  nixos_system_attr      = "/home/gumbo/nixos#nixosConfigurations.k3-1.config.system.build.toplevel"
+  nixos_partitioner_attr = "/home/gumbo/nixos#nixosConfigurations.k3-1.config.system.build.diskoScript"
+  target_host            = local.k3-1_ipv4
+  instance_id            = local.k3-1_ipv4
+  debug_logging          = true
+  build_on_remote        = false
+  stop_after_disko       = true
+  install_ssh_key        = file("/home/gumbo/.ssh/temp")
+}
