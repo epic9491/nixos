44522e7e631a7085f3d3b726c672451db99b0932
secret-mgmt <codeberg@gaialabs.space>
2026-05-22 22:49:51 -0500

    add k3 token file

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,9 @@ let
   secret-mgmt = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDMvYIo3MxF2XpAhMjZ/T6NfI+PAlB8GDrZ11xjH5uVb gumbo@nixos";
   zeus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/xhPKIy6eFgVXgMPsGkzjDUxuJJP3oEWutvK49lIPu gumbo@zeus";
   systems = [ secret-mgmt zeus ];
+  k3s = [ secret-mgmt zeus ];
 in
 {
   "test.age".publicKeys = systems;
+  "k3s-token.age".publicKeys = k3s;
 }
