ff7fac523be012ea58a51720a27bbee41a73a34c
secret-mgmt <codeberg@gaialabs.space>
2026-05-23 10:55:37 -0500

    add k3 agent pub keys to agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,12 @@
 let
   secret-mgmt = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDMvYIo3MxF2XpAhMjZ/T6NfI+PAlB8GDrZ11xjH5uVb gumbo@nixos";
   zeus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/xhPKIy6eFgVXgMPsGkzjDUxuJJP3oEWutvK49lIPu gumbo@zeus";
-  systems = [ secret-mgmt zeus ];
-  k3s = [ secret-mgmt zeus ];
+  k3-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF4AawMOe7HKxlu1phyLc7iDyWIVFdhoeZEffzaNqdfr gumbo@k3-1";
+  k3-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIcYW42Db38mYMDZ0VgTjAX/V6wnla4yleOEWggL7a16 gumbo@k3-2";
+  all-systems = [ secret-mgmt zeus k3-1 k3-2];
+  k3s = [ secret-mgmt zeus k3-1 k3-2 ];
 in
 {
-  "test.age".publicKeys = systems;
+  "test.age".publicKeys = all-systems;
   "k3s-token.age".publicKeys = k3s;
 }
