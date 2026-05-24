915bea1c5b6d9a9bca132d32b2cdd7fb6c9d07fa
zeus <codeberg@gaialabs.space>
2026-05-23 20:18:33 -0500

    fix conflict with headlamp chart

--- devices/server/zeus/kubes.nix
+++ devices/server/zeus/kubes.nix
@@ -12,7 +12,9 @@
     role = "server";
     clusterInit = true;
     nodeTaint = [ "CriticalAddonsOnly=true:NoExecute" ];
-
+    extraFlags = toString [
+      "--write-kubeconfig-mode=644"
+    ];
     autoDeployCharts = {
       headlamp = {
         enable = true;
@@ -30,21 +32,19 @@
         };
       };
     };
-
     manifests = {
-      headlamp-admin-sa.content = {
+      headlamp-user-sa.content = {
         apiVersion = "v1";
         kind = "ServiceAccount";
         metadata = {
-          name = "headlamp-admin";
+          name = "headlamp-user";
           namespace = "kube-system";
         };
       };
-
-      headlamp-admin-binding.content = {
+      headlamp-user-binding.content = {
         apiVersion = "rbac.authorization.k8s.io/v1";
         kind = "ClusterRoleBinding";
-        metadata.name = "headlamp-admin";
+        metadata.name = "headlamp-user";
         roleRef = {
           apiGroup = "rbac.authorization.k8s.io";
           kind = "ClusterRole";
@@ -52,31 +52,28 @@
         };
         subjects = [{
           kind = "ServiceAccount";
-          name = "headlamp-admin";
+          name = "headlamp-user";
           namespace = "kube-system";
         }];
       };
-
-      headlamp-admin-token.content = {
+      headlamp-user-token.content = {
         apiVersion = "v1";
         kind = "Secret";
         metadata = {
-          name = "headlamp-admin-token";
+          name = "headlamp-user-token";
           namespace = "kube-system";
-          annotations."kubernetes.io/service-account.name" = "headlamp-admin";
+          annotations."kubernetes.io/service-account.name" = "headlamp-user";
         };
         type = "kubernetes.io/service-account-token";
       };
     };
   };
-
   networking.firewall.allowedUDPPorts = [ 8472 ]; # Flannel
   networking.firewall.allowedTCPPorts = [
     6443  # k3s API server
     10250 # kubelet
     30000 # headlamp
   ];
-
   age.secrets."k3s-token.age" = {
     file = ../../../secrets/k3s-token.age;
     path = "/run/secrets/k3s-token";
