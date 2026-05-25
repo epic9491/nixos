632584fb6aa7806fa69ccbe10bb4f46616eea1a6
sensei <codeberg@gaialabs.space>
2026-05-24 22:07:59 -0500

    add ts auth-key file to k3s agent module

--- modules/k3s-agent.nix
+++ modules/k3s-agent.nix
@@ -19,6 +19,8 @@ in
   config = lib.mkIf cfg.enable {
     networking.firewall.allowedUDPPorts = [ 8472 ]; # Flannel
 
+    services.tailscale.authKeyFile = "/run/secrets/k3s-ts-auth";
+    
     services.k3s = {
       enable = true;
       role = "agent";
@@ -30,6 +32,14 @@ in
     age.secrets."k3s-token.age" = {
       file = ../secrets/k3s-token.age;
       path = "/run/secrets/k3s-token";
+      owner = "root";
+      group = "root";
+      mode = "0400";
+    };
+    
+    age.secrets."k3s-ts-auth.age" = {
+      file = ../secrets/k3s-ts-auth.age;
+      path = "/run/secrets/k3s-ts-auth";
       owner = "root";
       group = "root";
       mode = "0400";
