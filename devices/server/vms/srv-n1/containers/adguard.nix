bcf37e96298f5b0f72491fd4d4f4370ea31fdd16
sensei <codeberg@gaialabs.space>
2026-07-03 15:28:58 -0500

    switch adguard ports and redirect rules for 5353 --> 53

--- devices/server/vms/srv-n1/containers/adguard.nix
+++ devices/server/vms/srv-n1/containers/adguard.nix
@@ -9,11 +9,11 @@
         autoStart = true;
         autoUpdate = "registry";
         ports = [
-          "100.69.69.210:53:53/tcp"
-          "100.69.69.210:53:53/udp"
-          "[fd7a:115c:a1e0::7e36:ab2a]:53:53/tcp"
-          "[fd7a:115c:a1e0::7e36:ab2a]:53:53/udp"
-          "127.0.0.1:80:80"
+          "100.69.69.210:5353:53/tcp"
+          "100.69.69.210:5353:53/udp"
+          "[fd7a:115c:a1e0::7e36:ab2a]:5353:53/tcp"
+          "[fd7a:115c:a1e0::7e36:ab2a]:5353:53/udp"
+          "127.0.0.1:8080:80"
         ];
         volumes = [
           "/var/lib/adguard/work:/opt/adguardhome/work:Z"
@@ -23,4 +23,18 @@
       };
     };
   };
+  
+  # Redirect traffic from 5353 to 53 since adguard cant bind to a privileged port
+  networking.firewall.extraCommands = ''
+    iptables -t nat -A PREROUTING -d 100.69.69.210 -p udp --dport 53 -j REDIRECT --to-port 5353
+    iptables -t nat -A PREROUTING -d 100.69.69.210 -p tcp --dport 53 -j REDIRECT --to-port 5353
+    ip6tables -t nat -A PREROUTING -d fd7a:115c:a1e0::7e36:ab2a -p udp --dport 53 -j REDIRECT --to-port 5353
+    ip6tables -t nat -A PREROUTING -d fd7a:115c:a1e0::7e36:ab2a -p tcp --dport 53 -j REDIRECT --to-port 5353
+  '';
+  networking.firewall.extraStopCommands = ''
+    iptables -t nat -D PREROUTING -d 100.69.69.210 -p udp --dport 53 -j REDIRECT --to-port 5353 || true
+    iptables -t nat -D PREROUTING -d 100.69.69.210 -p tcp --dport 53 -j REDIRECT --to-port 5353 || true
+    ip6tables -t nat -D PREROUTING -d fd7a:115c:a1e0::7e36:ab2a -p udp --dport 53 -j REDIRECT --to-port 5353 || true
+    ip6tables -t nat -D PREROUTING -d fd7a:115c:a1e0::7e36:ab2a -p tcp --dport 53 -j REDIRECT --to-port 5353 || true
+  '';
 }
