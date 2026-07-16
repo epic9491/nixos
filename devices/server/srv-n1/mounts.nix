31b5abd3e2f28739543111107d857806e9e44749
sensei <codeberg@gaialabs.space>
2026-07-15 22:53:17 -0500

    [skip ci] srv-n1: adjust NFS mounts

--- devices/server/srv-n1/mounts.nix
+++ devices/server/srv-n1/mounts.nix
@@ -1,6 +1,6 @@
 {
   fileSystems."/mnt/manga" = {
-    device = "192.168.0.183:/mnt/jelly/manga";
+    device = "192.168.0.2:/mediapool/media/manga";
     fsType = "nfs";
     options = [
       "noatime"
@@ -8,7 +8,7 @@
     ];
   };
   fileSystems."/mnt/photos" = {
-    device = "192.168.0.183:/mnt/jelly/photos";
+    device = "192.168.0.2:/mediapool/media/photos";
     fsType = "nfs";
     options = [
       "noatime"
@@ -16,7 +16,7 @@
     ];
   };
   fileSystems."/mnt/music" = {
-    device = "192.168.0.183:/mnt/jelly/music/lossless";
+    device = "192.168.0.2:/mediapool/media/music/lossless";
     fsType = "nfs";
     options = [
       "noatime"
