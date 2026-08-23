6007e1d32c919509742b13d353c6a9ab1ddca107
sensei <codeberg@gaialabs.space>
2026-08-22 22:55:01 -0500

    change container port in filebrowser and user id

--- machines/server/srv-n1/containers/filebrowser.nix
+++ machines/server/srv-n1/containers/filebrowser.nix
@@ -8,7 +8,8 @@
       containers.filebrowser = {
         image = "ghcr.io/gtsteffaniak/filebrowser:1.5.3-stable@sha256:e2ac55ccbe53d63b3f1d7d5ea5b82edf589b005a4b747b59912f97c6ba4f969e";
         autoStart = true;
-        ports = [ "127.0.0.1:8090:80" ];
+        ports = [ "127.0.0.1:8090:8080" ];
+        userNS = "keep-id:uid=1000,gid=1000";
         volumes = [ "/var/lib/filebrowser/data:/home/filebrowser/data" ];
         extraConfig = {
           Container = {
